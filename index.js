96fcb82d3efefc91c1e34f107b097dc0fa2d8674
João Donato <joaod.morais@gmail.com>
2023-01-31 09:46:23 -0300

    trabalhando com metodos assincronos

--- index.js
+++ index.js
@@ -3,18 +3,26 @@ import chalk from 'chalk';
 import fs from 'fs';
 
 function trataErro (erro) {
-    throw new Error(erro);
+    throw new Error(chalk.redBright(erro.code, 'não foi encontrado o arquivo no diretório'));
 }
-
-function pegaArquivo (caminhoDoArquivo){
+// ***código assíncrono***
+function pegaArquivo(caminhoDoArquivo){
     const encoding = 'utf-8';
-    fs.readFile(caminhoDoArquivo, encoding, (erro, texto) => {
-        if(erro){
-            trataErro(erro);
-        }
-
-        console.log(chalk.bgGray(texto));
-    })
+    fs.promises
+    .readFile(caminhoDoArquivo, encoding)
+    .then((texto) => console.log(chalk.green(texto)))
+    .catch(trataErro)
 }
+// ***código síncrono***
+// function pegaArquivo (caminhoDoArquivo){
+//     const encoding = 'utf-8';
+//     fs.readFile(caminhoDoArquivo, encoding, (erro, texto) => {
+//         if(erro){
+//             trataErro(erro);
+//         }
+
+//         console.log(chalk.bgGray(texto));
+//     })
+//}
 
 pegaArquivo('./arquivos/texto.md');
