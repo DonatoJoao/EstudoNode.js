80fb6996eac4b543f8ed17f6d3111865fa5aa863
João Donato <joaod.morais@gmail.com>
2023-02-02 18:05:57 -0300

    Construindo os objetos com as validações

--- src/http-validacao.js
+++ src/http-validacao.js
@@ -1,4 +1,4 @@
-
+import chalk from  'chalk';
 
 function extraiLinks (arrLinks){
     return arrLinks.map((objetoLink) => Object.values(objetoLink).join());
@@ -6,20 +6,36 @@ function extraiLinks (arrLinks){
 }
 
 async function checaStatus(listaURLs){
-    const arrStatus = await Promise.all(
+    const arrStatus = await Promise
+    .all(
         listaURLs.map(async(url) => {
-        const response = await fetch(url)
-        return response.status;
+            try {
+                const response = await fetch(url)
+                return response.status;
+            } catch (erro) {
+                return manejaErros(erro);
+            }
         })
     )
     return arrStatus;
 }
 
+function manejaErros(erro) {
+    if (erro.cause.code === 'ENOTFOUND'){
+        return 'Link não encontrado';
+    } else {
+        return 'Algo deu errado';
+    }
+}
 
 export default async function listaValidada (listaDeLinks) {
     const links = extraiLinks(listaDeLinks);
     const status = await checaStatus(links);
-    return status;
+    
+    return listaDeLinks.map((objeto, indice) => ({
+        ...objeto,
+        status: status[indice]
+    })) 
 }
 
 
