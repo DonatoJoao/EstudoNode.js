dc386737669bf58354753977265c3d8db9edd829
João Donato <joaod.morais@gmail.com>
2023-02-02 09:10:01 -0300

    processando diretórios e tratando erros

--- src/cli.js
+++ src/cli.js
@@ -1,13 +1,37 @@
 import pegaArquivo from './index.js';
 import chalk from 'chalk'; 
-
+import fs from 'fs';
 
 const caminho = process.argv;
 
-async function processaTexto (caminho) {
-    const resultado = await pegaArquivo(caminho[2]);
-    console.log(chalk.yellow('Lista de links'), resultado);
+function imprimeLista (resultado, indentificador = '') {
+    console.log(chalk.yellow('Lista de links'),
+    chalk.black.bgGreen(indentificador),
+    resultado);
+}
 
+async function processaTexto (argumentos) {
+    const caminho = argumentos[2];
+
+    try {
+        fs.lstatSync(caminho);
+    } catch (erro) {
+        if (erro.code === 'ENOENT'){
+            console.log('Arquivo ou diretório não existe');
+            return;
+        }
+    }
+
+    if (fs.lstatSync(caminho).isFile()){
+        const resultado = await pegaArquivo(argumentos[2]);
+        imprimeLista(resultado);        
+    } else if (fs.lstatSync(caminho).isDirectory()){
+        const arquivos = await fs.promises.readdir(caminho)
+        arquivos.forEach(async(nomeDeArquivo) => {
+        const lista = await pegaArquivo(`${caminho}/${nomeDeArquivo}`)
+        imprimeLista(lista, nomeDeArquivo)
+        })
+    }
 }
 
 processaTexto(caminho);
